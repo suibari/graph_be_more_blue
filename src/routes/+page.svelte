4f07e56eda0d68801e1c6780b165d645e8bb83ea
suibari <404.not.found.address@gmail.com>
2025-09-17 01:52:53 +0900

    タイトル修正

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -48,7 +48,7 @@
 </script>
 
 <div class="container mx-auto p-4">
-  <h1 class="text-3xl font-bold mb-4">Bluesky Graph</h1>
+  <h1 class="text-3xl font-bold mb-4">GraphBeMoreBlue!</h1>
   {#if graphData}
     <Graph {graphData} on:nodeTap={handleNodeTap} />
   {:else}
