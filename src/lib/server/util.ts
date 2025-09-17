f3632328e75d33ba8c96e288b3b34f7885c8d4c3
suibari <404.not.found.address@gmail.com>
2025-09-17 21:48:20 +0900

    軽量化のためサムネイル画像を使用するよう修正

--- src/lib/server/util.ts
+++ src/lib/server/util.ts
@@ -1,6 +1,22 @@
 export async function imageToBase64(url: string): Promise<string | null> {
+  let imageUrl = url;
+  // 縮小版のURLを試す
+  if (url.includes('/avatar/')) {
+    const thumbnail_url = url.replace('/avatar/', '/avatar_thumbnail/');
+    try {
+      const response = await fetch(thumbnail_url);
+      if (response.ok) {
+        imageUrl = thumbnail_url;
+      } else {
+        console.warn(`Failed to fetch thumbnail from ${thumbnail_url}, trying original URL.`);
+      }
+    } catch (error) {
+      console.warn(`Error fetching thumbnail from ${thumbnail_url}, trying original URL.`, error);
+    }
+  }
+
   try {
-    const response = await fetch(url);
+    const response = await fetch(imageUrl);
     if (!response.ok) {
       console.error(`Failed to fetch image: ${response.statusText}`);
       return null;
